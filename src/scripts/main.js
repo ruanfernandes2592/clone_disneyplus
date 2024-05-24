a2aaa1d719c411e3f50432fe115a9aea6d814edb
ruanfernandes2592 <ruanfernandes2592@gmail.com>
2024-05-23 21:29:28 -0300

    Ajustes no mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('[data-tab-button]');
+    const buttons = document.querySelectorAll('[data-tab-button]'); // section shows
+    const questions = document.querySelectorAll('[data-faq-question]'); // section faq
+    const heroSection = document.querySelector('.hero'); // section hero
+
+    const alturaHero = heroSection.clientHeight; // header
+        window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
     
-    for (let i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) { // section shows - programação das abas
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -11,9 +23,20 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {  // section faq - accordion
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo() {
+function abreOuFechaResposta(elemento) {  // section faq - accordion
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
+
+function removeBotaoAtivo() { // section shows - programação das abas
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
@@ -21,10 +44,20 @@ function removeBotaoAtivo() {
     }
 }
 
-function esconteTodasAbas() {
+function esconteTodasAbas() { // section shows - programação das abas
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function ocultaElementosDoHeader() { // header
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
